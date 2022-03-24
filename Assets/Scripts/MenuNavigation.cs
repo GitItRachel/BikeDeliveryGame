1ba419dd098c7788f3b361028711144b2a9e9ef0
GetItRachel <malloner@oregonstate.edu>
2022-03-24 14:54:08 -0500

    Added homescreen and navigation UI

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+public class MenuNavigation : MonoBehaviour {
+    [SerializeField] private GameObject homepanel;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(homepanel);
+    }
+    
+    public void EnableHomePanel()
+    {
+        homepanel.SetActive(true);
+    }
+
+    public void DisableHomePanel()
+    {
+        homepanel.SetActive(!homepanel.activeSelf);
+    }
+}
